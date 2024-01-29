d037864194b116ca81f62796dbcc3bd4fb7e0579
Jasmin Ramirez <ms.jasmin1999@gmail.com>
2024-01-28 21:01:23 -0500

    Deployed changes and fixed lint issues

--- functions/index.js
+++ functions/index.js
@@ -8,43 +8,38 @@
  */
 
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
 const express = require("express");
 const cors = require("cors");
 
-const stripe = require("stripe") ('sk_test_51Od3BtA65hF8ZlgLHb8FSLjhXLOnfGCPXJ0jyUPxYnBUcPFiNKL9RBGGaiAHWjgQsLvE8ORpfUUlyIaXwFQ6LeUu00HBwcqw1n');
-
+// eslint-disable-next-line max-len
+const stripe = require("stripe")("sk_test_51Od3BtA65hF8ZlgLHb8FSLjhXLOnfGCPXJ0jyUPxYnBUcPFiNKL9RBGGaiAHWjgQsLvE8ORpfUUlyIaXwFQ6LeUu00HBwcqw1n");
 
 // API
-
-// App Config 
+// App Config
 const app = express();
 
 // Middlewares
-app.use(cors({origin:true}));
+app.use(cors({origin: true}));
 app.use(express.json());
 
-// API Routes 
-app.get('/', (request, response) => response.status(200).send('hello'));
+// API Routes
+app.get("/", (request, response) => response.status(200).send("hello"));
 
-app.post('/payments/create', async (request, response) => {
-    try {
-        const total = request.query.total;
-        console.log("Payments Request Recieved BOOM!! For this amount >>> ", total);
+app.post("/payments/create", async (request, response) => {
+  try {
+    const total = request.query.total;
+    console.log("Payments Request Recieved BOOM!! For this amount >>> ", total);
 
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: total, // subunites of the currency 
-            currency: "usd"
-        });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
 
-        response.json({ clientSecret: paymentIntent.client_secret });
-
-    } 
-    catch (error) {
-    console.error('Error creating payment intent:', error);
-    response.status(500).send('Internal Server Error');
-    }
+    response.json({clientSecret: paymentIntent.client_secret});
+  } catch (error) {
+    console.error("Error creating payment intent:", error);
+    response.status(500).send("Internal Server Error");
+  }
 });
 
 // Listen Command
--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5001/clone-c64a5/us-central1/api' // The api (cloud function) URL
+    baseURL: 'https://api-drhavio4na-uc.a.run.app'
+    // local host baseURL: 'http://127.0.0.1:5001/clone-c64a5/us-central1/api' // The api (cloud function) URL
 }); 
 
 export default instance;
