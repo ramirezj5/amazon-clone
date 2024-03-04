6211caa2864a2a582069a0cafcf271181afd8438
Jasmin Ramirez <ms.jasmin1999@gmail.com>
2024-03-03 20:35:17 -0500

    Add firebase file

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore'; // Import Firestore if you're using it
+import { getAuth } from 'firebase/auth'; // Import Authentication if you're using it
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID ,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID ,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID 
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize the database 
+  const db = getFirestore(app); // Firestore
+  const auth = getAuth(app); // Authentication
+
+  export {db, auth};
